ba74308d620395e525246754b747273914be6630
spencereasybutton <128197048+spencereasybutton@users.noreply.github.com>
2026-04-24 12:38:30 -0700

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>EBS Dashboard is Live 🚀</h1>
-      <p>If you see this, your deployment works.</p>
-    </div>
-  );
-}
