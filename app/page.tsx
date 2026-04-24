449e1c41fd2c77de4474e4e3a5080cd58fc16067
spencereasybutton <128197048+spencereasybutton@users.noreply.github.com>
2026-04-24 12:34:42 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>EBS Dashboard is Live 🚀</h1>
+      <p>If you see this, your deployment works.</p>
+    </div>
+  );
+}
