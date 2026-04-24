96f55968f0df5df069ce8b994e73733f4d6e7f71
spencereasybutton <128197048+spencereasybutton@users.noreply.github.com>
2026-04-24 12:38:00 -0700

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "EBS Dashboard",
+  description: "Easy Button Software Platform"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
