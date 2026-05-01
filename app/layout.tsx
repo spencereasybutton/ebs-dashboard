bdd22cdf5de0e6c08e401d6a535a1cfdec06aa84
spencereasybutton <128197048+spencereasybutton@users.noreply.github.com>
2026-05-01 13:08:33 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body style={{ margin: 0 }}>{children}</body>
+        <body style={{ margin: 0, background: "#050505" }}>{children}</body>
       </html>
     </ClerkProvider>
   );
